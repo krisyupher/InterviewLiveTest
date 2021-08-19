70e17fa89615aaaf84c3be5b85a9234681d39980
Cristian Florez <48220112+krisyupher@users.noreply.github.com>
2021-08-19 08:23:31 -0500

    animationBebidas

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const logosContainer = document.getElementById("logosContainer");
-console.log(logosContainer);
+let aux = 30;
+let html = document.getElementById("html");
+let imgBebida = document.getElementsByClassName("imgBebida");
+window.onscroll = function (e) {
+  var st = window.pageYOffset || html.scrollTop;
+  if (st > 0) {
+    aux -= 5;
+    if (aux < 0) {
+      aux = 0;
+    }
+  } else {
+    aux += 30;
+  }
+  imgBebida[0].style.marginRight = aux + "rem";
+  imgBebida[1].style.marginRight = aux + "rem";
+  imgBebida[2].style.marginRight = aux + "rem";
+};
